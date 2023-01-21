82f74bd1e2589af7751577d8035109e753ad5686
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-21 21:22:53 +0100

    create migrations

---
+++ database/migrations/2023_01_21_195327_create_likes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('liked_by_user_id');
+            $table->unsignedBigInteger('liked_user_id');
+            $table->timestamps();
+            $table->foreign('liked_by_user_id')->references('id')->on('users')->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreign('liked_user_id')->references('id')->on('users')->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
