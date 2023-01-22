5c46a4282ebf48849a244ebe4c858f65e429d584
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-22 22:23:55 +0100

    add custom 404 api error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,6 +46,11 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function (NotFoundHttpException $exception, $request) {
+            if ($request->is('api/*')) {
+                return response()->json('Not found.', 404);
+            }
         });
     }
 }
